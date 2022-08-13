24a9b4c90eeb886d9d8056e3052b4f02059ce966
royce-reed <rjreed1@gmail.com>
2022-08-13 09:29:58 -0500

    updated coming soon page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 // import southerns_logo from './southerns_logo.svg';
-import logo_southerns from './logo_southerns.svg';
+import logo_southerns from './imgs/logo_southerns.svg';
 import './App.css';
 import { FaInstagram, FaFacebook } from 'react-icons/fa';
 
@@ -10,18 +10,18 @@ function App() {
       <header className='App-header'>
         <img src={logo_southerns} className='App-logo' alt='logo' />
         <h1>Coming Soon</h1>
-        <div className='social-media'>
+        <div>
           <a
             href='https://www.instagram.com/southernsfood/'
             target='_blank'
             rel='noopener noreferrer'>
-            <FaInstagram style={{ height: '2em', width: '2em' }} />
+            <FaInstagram className='social-media' />
           </a>
           <a
             href='https://www.facebook.com/Southerns-509576972842884'
             target='_blank'
             rel='noopener noreferrer'>
-            <FaFacebook style={{ height: '2em', width: '2em' }} />
+            <FaFacebook className='social-media' />
           </a>
         </div>
       </header>
