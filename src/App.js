65690bba9246caef702885d42ace293012e51037
royce-reed <rjreed1@gmail.com>
2022-08-12 20:31:50 -0500

    coming soon page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import southerns_logo from './southerns_logo.svg';
+import logo_southerns from './logo_southerns.svg';
 import './App.css';
+import { FaInstagram, FaFacebook } from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo_southerns} className='App-logo' alt='logo' />
+        <h1>Coming Soon</h1>
+        <div className='social-media'>
+          <a
+            href='https://www.instagram.com/southernsfood/'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <FaInstagram style={{ height: '2em', width: '2em' }} />
+          </a>
+          <a
+            href='https://www.facebook.com/Southerns-509576972842884'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <FaFacebook style={{ height: '2em', width: '2em' }} />
+          </a>
+        </div>
       </header>
     </div>
   );
