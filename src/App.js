a94168b8f0e4fbf6497c49ab60ec7da95896ed4f
royce-reed <rjreed1@gmail.com>
2022-08-21 16:26:45 -0500

    update packages, static assets, and SEO

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import ComingSoon from './Components/ComingSoon';
-import './App.css';
+import { useSelector } from 'react-redux';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import lightTheme from './themes/lightTheme.js';
+import darkTheme from './themes/darkTheme.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TopNavBar from './Components/TopNavBar';
+import Home from './Pages/Home.js';
+import Login from './Pages/Login.js';
+import './styles/App.css';
 
 function App() {
+  const theme = useSelector((state) => state.theme.value);
+
+  const defaultContainer = (
+    <>
+      <TopNavBar />
+      <div className='App'>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </div>
+    </>
+  );
+
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <ComingSoon />
-      </header>
-    </div>
+    <Router>
+      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+        <CssBaseline />
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='*' element={defaultContainer} />
+        </Routes>
+      </ThemeProvider>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './state/store.js';
 import App from './App';
+// import TopNavBar from './Components/TopNavBar';
+
+
+import './styles/index.css';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
+
     <App />
+    </Provider>
   </React.StrictMode>
 );
