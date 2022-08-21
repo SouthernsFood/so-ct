dfb7500ac0df3e6e016159b344ba701adac02644
royce-reed <rjreed1@gmail.com>
2022-08-21 16:24:42 -0500

    added some basic styling

---
+++ src/themes/darkTheme.js
@@ -0,0 +1,12 @@
+import createTheme from '@mui/material/styles/createTheme.js';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  typography: {
+    fontFamily: 'monospace',
+  },
+});
+
+export default darkTheme;
---
+++ src/themes/lightTheme.js
@@ -0,0 +1,12 @@
+import createTheme from '@mui/material/styles/createTheme.js';
+
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+  typography: {
+    fontFamily: 'monospace',
+  },
+});
+
+export default lightTheme;
