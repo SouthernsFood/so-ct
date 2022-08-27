497bfa94cd3e2ccf62755da5a330386c23ebe6e3
royce-reed <rjreed1@gmail.com>
2022-08-27 16:22:50 -0500

    added logic for edit menu item modal

--- src/Components/CMS/Menu/EditMenuItemModal.js
+++ src/Components/CMS/Menu/EditMenuItemModal.js
@@ -7,7 +7,7 @@ import CloseIcon from '@mui/icons-material/Close';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { toast } from 'react-toastify';
-import { update, getMenu, reset } from '../../../state/features/menu/menuSlice';
+import { update, getMenu, deleteItem, reset } from '../../../state/features/menu/menuSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
@@ -40,7 +40,20 @@ const EditMenuItemModal = forwardRef(({ handleClose, menuItem }, ref) => {
     }
   };
 
+  const handleDelete = () => {
+    try {
+      if (window.confirm('Are you sure you want to delete this item?')) {
+        dispatch(deleteItem(menuItemObject));
+        toast.success('Menu item deleted');
+        dispatch(getMenu());
+        handleClose();
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
+  style.overflowY = 'auto';
 
   return (
     <Box sx={style} ref={ref} tabIndex={-1}>
@@ -52,47 +65,63 @@ const EditMenuItemModal = forwardRef(({ handleClose, menuItem }, ref) => {
         }}
       />
       <Divider />
-      <Typography id='modal-modal-title' variant='h6' component='h2' style={{ marginBottom: '2rem' }}>
+      <Typography
+        id='modal-modal-title'
+        variant='h6'
+        component='h2'
+        style={{ marginBottom: '2rem' }}>
         Edit Menu Item
       </Typography>
       <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        {
-          Object.keys(menuItem)
-            .slice(0, -1)
-            .map(key => {
-              if (key === 'id' ) return null;
-              if (key === 'featured') {
-                return (
-                  <Fragment key={key}>
-                    <Box style={{ marginTop: '1rem' }}>
-                      <FormControl component='fieldset'>
-                        <FormLabel component='legend'>Featured</FormLabel>
-                        <RadioGroup row aria-label='featured' name='featured' value={menuItemObject.featured} onChange={(e) => setMenuItemObject({ ...menuItemObject, [key]: e.target.value })}>
-                          <FormControlLabel value='true' control={<Radio />} label='Yes' />
-                          <FormControlLabel value='false' control={<Radio />} label='No' />
-                        </RadioGroup>
-                      </FormControl>
-                    </Box>
-                  </Fragment>
-                );
-              }
+        {Object.keys(menuItem)
+          .slice(0, -1)
+          .map((key) => {
+            if (key === 'id') return null;
+            if (key === 'featured') {
               return (
-                <TextField
-                  style={{ margin: '10px', width: '95%', maxWidth: '1000px' }}
-                  key={key}
-                  id={key}
-                  label={key}
-                  value={menuItemObject[key]}
-                  onChange={(e) => setMenuItemObject({ ...menuItemObject, [key]: e.target.value })}
-                />
+                <Fragment key={key}>
+                  <Box style={{ marginTop: '1rem' }}>
+                    <FormControl component='fieldset'>
+                      <FormLabel component='legend'>Featured</FormLabel>
+                      <RadioGroup
+                        row
+                        aria-label='featured'
+                        name='featured'
+                        value={menuItemObject.featured}
+                        onChange={(e) =>
+                          setMenuItemObject({ ...menuItemObject, [key]: e.target.value })
+                        }>
+                        <FormControlLabel value='true' control={<Radio />} label='Yes' />
+                        <FormControlLabel value='false' control={<Radio />} label='No' />
+                      </RadioGroup>
+                    </FormControl>
+                  </Box>
+                </Fragment>
               );
-            })
-        }
-        <Button variant='contained' onClick={handleSubmit} style={{ marginTop: '1rem' }}>
-          Submit
+            }
+            return (
+              <TextField
+                style={{ margin: '10px', width: '95%', maxWidth: '1000px' }}
+                key={key}
+                id={key}
+                label={key}
+                value={menuItemObject[key]}
+                onChange={(e) => setMenuItemObject({ ...menuItemObject, [key]: e.target.value })}
+              />
+            );
+          })}
+        <Button variant='contained' onClick={handleSubmit} style={{ marginTop: '1rem', width: '55%', maxWidth: '1000px', height: '50px' }}>
+          Submit Changes
+        </Button>
+
+        <Button
+          variant='contained'
+          color='error'
+          onClick={handleDelete}
+          style={{ marginTop: '1rem', width: '55%', maxWidth: '1000px', height: '50px' }}>
+          Delete Menu Item
         </Button>
       </Box>
-
     </Box>
   );
 });
